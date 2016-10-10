bd17ebe2f4c0c0eb251f6b459d512f3316591127
Tolik <talibasya@gmail.com>
2016-10-10 17:53:41 +0200

    fix conflicts

--- routes/NewProductSeller/index.js
+++ routes/NewProductSeller/index.js
@@ -98,84 +98,41 @@ export default class NewProductSeller extends Component {
 						<Image source={{uri: 'https://unsplash.it/600/100?image=147'}} style={st.imgTitle}/>
 						<Text style={ st.titleText } >SHOP NAME</Text>
 					</View>
-<<<<<<< HEAD
 						
-					<View style={st.contentWrap}>
-						<Text style={st.blockTitle} >ADD NEW PRODUCT</Text>
-						
-
-						<Text style={st.textInput} >Product name</Text>
-						<TextInput 
-							style={st.input}
-							onChange={ this.changeProductData.bind(this, 'productName') }
-							placeholder={'Mens Brogue Shoe'} 
-							underlineColorAndroid="#edb4ff" 
-							placeholderTextColor='#cccccc'
-							value={ productName }
-							/>
-
-						<Text style={st.textInput} >Category</Text>
-						
-						<TextInput 
-							style={st.input} 
-							onChange={ this.changeProductData.bind(this, 'category') }
-							placeholder={'Shoes'} 
-							placeholderTextColor='#cccccc'
-							value={ category }
-							underlineColorAndroid="#edb4ff" 
-							/>
-
-						<Text style={st.textInput} >Description</Text>
-						
-						<View style={st.inputMultiWrap}>
-							<TextInput 
-								style={st.inputMulti} 
-								onChange={ this.changeProductData.bind(this, 'description') }
-								placeholder={'Populated by my facebook profile'} 
-								multiline = {true} 
-								placeholderTextColor='#b6b6b6'
-								underlineColorAndroid="transparent"
-								value={ description }
-								numberOfLines = {4} />
-						</View>
-					</View>
-=======
->>>>>>> e33c36d96847e0e3f77925dc0324a93374f91ab8
-
 					<Card>
 						<Card.Body>
 							<Text style={st.blockTitle} >ADD NEW PRODUCT</Text>
-
+							
 
 							<Text style={st.textInput} >Product name</Text>
-							<TextInput
+							<TextInput 
 								style={st.input}
-								onEndEditing={ this.changeProductData.bind(this, 'productName') }
-								placeholder={'Mens Brogue Shoe'}
-								underlineColorAndroid="#edb4ff"
+								onChange={ this.changeProductData.bind(this, 'productName') }
+								placeholder={'Mens Brogue Shoe'} 
+								underlineColorAndroid="#edb4ff" 
 								placeholderTextColor='#cccccc'
 								value={ productName }
 								/>
 
 							<Text style={st.textInput} >Category</Text>
-
-							<TextInput
-								style={st.input}
-								onEndEditing={ this.changeProductData.bind(this, 'category') }
-								placeholder={'Shoes'}
+							
+							<TextInput 
+								style={st.input} 
+								onChange={ this.changeProductData.bind(this, 'category') }
+								placeholder={'Shoes'} 
 								placeholderTextColor='#cccccc'
 								value={ category }
-								underlineColorAndroid="#edb4ff"
+								underlineColorAndroid="#edb4ff" 
 								/>
 
 							<Text style={st.textInput} >Description</Text>
-
+							
 							<View style={st.inputMultiWrap}>
-								<TextInput
-									style={st.inputMulti}
-									onEndEditing={ this.changeProductData.bind(this, 'description') }
-									placeholder={'Populated by my facebook profile'}
-									multiline = {true}
+								<TextInput 
+									style={st.inputMulti} 
+									onChange={ this.changeProductData.bind(this, 'description') }
+									placeholder={'Populated by my facebook profile'} 
+									multiline = {true} 
 									placeholderTextColor='#b6b6b6'
 									underlineColorAndroid="transparent"
 									value={ description }
@@ -203,81 +160,66 @@ export default class NewProductSeller extends Component {
 
 					<Card>
 						<Card.Body>
+
+							<Text style={st.textInput} >Quantity in Stock</Text>
+							
+							<TextInput 
+								style={st.input}
+								onChange={ this.changeProductData.bind(this, 'quantity') }
+								placeholder={'456'} 
+								underlineColorAndroid="#edb4ff" 
+								placeholderTextColor='#cccccc'
+								value={ quantity } />
+
+							<Text style={st.textInput} >Retail Price $</Text>
+							
+							<TextInput 
+								style={st.input}
+								onChange={ this.changeProductData.bind(this, 'reatilPrice') }
+								placeholder={'89'} 
+								underlineColorAndroid="#edb4ff" 
+								placeholderTextColor='#cccccc'
+								value={ reatilPrice } />
+
+							<Text style={st.textInput} >Cost Price $</Text>
+							
+							<TextInput 
+								style={st.input}
+								onChange={ this.changeProductData.bind(this, 'costPrice') }
+								placeholder={'56'} 
+								underlineColorAndroid="#edb4ff" 
+								placeholderTextColor='#cccccc'
+								value={ costPrice } />
+
+							<Text style={st.textInput} >VAT %</Text>
+							
+							<TextInput 
+								style={st.input}
+								onChange={ this.changeProductData.bind(this, 'vat') }
+								placeholder={'4.5'} 
+								underlineColorAndroid="#edb4ff" 
+								placeholderTextColor='#cccccc'
+								value={ vat } />
+
+							<Text style={st.textInput} >Supplier</Text>
+							
+							<TextInput 
+								style={st.input}
+								onChange={ this.changeProductData.bind(this, 'supplier') }
+								placeholder={'Bescot Shoes'} 
+								underlineColorAndroid="#edb4ff" 
+								placeholderTextColor='#cccccc'
+								value={ supplier } />
+						
 							<TouchableNativeFeedback
 							  onPress={this.onSendProduct} >
-							  <View style={st.purpleButtonView} >
-							    <Text style={st.purpleButtonName} >SAVE ADDRESS</Text>
-							  </View>
+								<View style={st.purpleButtonView} >
+							    	<Text style={st.purpleButtonName} >SAVE PRODUCT</Text>
+								</View>
 							</TouchableNativeFeedback>
-<<<<<<< HEAD
-						</View>
 
-					</View>
-
-					<View style={st.contentWrap}>
-
-						<Text style={st.textInput} >Quantity in Stock</Text>
-						
-						<TextInput 
-							style={st.input}
-							onChange={ this.changeProductData.bind(this, 'quantity') }
-							placeholder={'456'} 
-							underlineColorAndroid="#edb4ff" 
-							placeholderTextColor='#cccccc'
-							value={ quantity } />
-
-						<Text style={st.textInput} >Retail Price $</Text>
-						
-						<TextInput 
-							style={st.input}
-							onChange={ this.changeProductData.bind(this, 'reatilPrice') }
-							placeholder={'89'} 
-							underlineColorAndroid="#edb4ff" 
-							placeholderTextColor='#cccccc'
-							value={ reatilPrice } />
-
-						<Text style={st.textInput} >Cost Price $</Text>
-						
-						<TextInput 
-							style={st.input}
-							onChange={ this.changeProductData.bind(this, 'costPrice') }
-							placeholder={'56'} 
-							underlineColorAndroid="#edb4ff" 
-							placeholderTextColor='#cccccc'
-							value={ costPrice } />
-
-						<Text style={st.textInput} >VAT %</Text>
-						
-						<TextInput 
-							style={st.input}
-							onChange={ this.changeProductData.bind(this, 'vat') }
-							placeholder={'4.5'} 
-							underlineColorAndroid="#edb4ff" 
-							placeholderTextColor='#cccccc'
-							value={ vat } />
-
-						<Text style={st.textInput} >Supplier</Text>
-						
-						<TextInput 
-							style={st.input}
-							onChange={ this.changeProductData.bind(this, 'supplier') }
-							placeholder={'Bescot Shoes'} 
-							underlineColorAndroid="#edb4ff" 
-							placeholderTextColor='#cccccc'
-							value={ supplier } />
-					
-						<TouchableNativeFeedback
-						  onPress={this.onSendProduct} >
-						  <View style={st.purpleButtonView} >
-						    <Text style={st.purpleButtonName} >SAVE PRODUCT</Text>
-						  </View>
-						</TouchableNativeFeedback>
-					</View>
-=======
 						</Card.Body>
 					</Card>
->>>>>>> e33c36d96847e0e3f77925dc0324a93374f91ab8
-
 
 				</ScrollView>
 

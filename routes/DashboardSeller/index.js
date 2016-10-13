41ad3e6520367aca23c7d6040434c84d6adbc966
Tolik <talibasya@gmail.com>
2016-10-13 11:51:45 +0200

    #13 styled profile part, add and style deal of the day block, best sellers, featured store

--- routes/DashboardSeller/index.js
+++ routes/DashboardSeller/index.js
@@ -40,36 +40,175 @@ export default class DashboardSeller extends Component {
 
 					    <View style={dashSellerStyle.shopperImgProfile}>
 					    	<Image source={{uri: `http://graph.facebook.com/298288423897442/picture?type=large` }} style={st.shopProfileImg}/>
-					    	<Text >FirstName LastName</Text>
+					    	<Text style={ dashSellerStyle.shopperName } >FirstName LastName</Text>
 					    </View>
 
 					    <View style={dashSellerStyle.shopperPropsProfile} >
 						    <View style={dashSellerStyle.alignCenter} >
-						    	<Text>106</Text>
+						    	<Text style={dashSellerStyle.textPropValue } >106</Text>
 						    </View>
 						    
-						    <View style={dashSellerStyle.alignCenter} >
-						    	<Text>Items Bought</Text>
-						    </View>
+					    	<Text style={dashSellerStyle.textPropDesc} >Items Bought</Text>
 					    </View>
 
 					    <View style={dashSellerStyle.shopperPropsProfile} >
-						    <Text>47{`\n`}</Text>
-						    <Text>Items Bought</Text>
+						    <View style={dashSellerStyle.alignCenter} >
+						    	<Text style={dashSellerStyle.textPropValue } >47</Text>
+						    </View>
+						    
+						    <Text style={dashSellerStyle.textPropDesc} >Items Bought</Text>
 					    </View>
 
 				        <View style={dashSellerStyle.shopperPropsProfile} >
-				    	    <Text>23{`\n`}</Text>
-				    	    <Text>Items Bought</Text>
+						    <View style={dashSellerStyle.alignCenter} >
+				    	    	<Text style={dashSellerStyle.textPropValue } >23</Text>
+						    </View>
+				    	    
+				    	    <Text style={dashSellerStyle.textPropDesc} >Items Bought</Text>
 				        </View>
 
 
 				  	</View>
 				</View>
 
-				<View>
+				<View style={dashSellerStyle.contentRow} >
+					
+					<View>
+						<Text style={dashSellerStyle.blockName}>DEAL OF THE DAY</Text>
+					</View>
+
+					<View style={dashSellerStyle.dealDay} >
+					   	<Image source={{uri: `https://unsplash.it/600/200?image=232` }} style={dashSellerStyle.dealDayImg}/>
+
+					   	<View style={dashSellerStyle.dealDayContent} >
+					   		<Text style={ { color: 'white', fontSize: 18 } } >AN ICON REMASTERED</Text>
+					   		<Text style={ { color: 'white',  fontSize: 12 } } >THE BEATS BY DR DRE STUDIO 2 OVER-EAR HEADPHONES</Text>
+
+							<TouchableNativeFeedback onPress={ () => { Alert.alert('buy now') } }>
+
+						   		<View style={ dashSellerStyle.dealDayButtonRow } >
+							   		<View style={ dashSellerStyle.dealDayButton } >
+							   			<Text style={ { color: 'white', fontSize: 10 } } >BUY NOW 20% OFF</Text>
+							   		</View>
+						   		</View>
+							
+							</TouchableNativeFeedback>
+
+					   	</View>
+					</View>
 
 				</View>
+
+
+				<View style={dashSellerStyle.contentRow}>
+					
+					<View>
+						<Text style={dashSellerStyle.blockName}>BEST SELLERS</Text>
+					</View>
+
+					<View style={dashSellerStyle.productList}>
+
+						<ScrollView horizontal={true}>
+
+							
+							<View style={dashSellerStyle.productItem} >
+							   		<Image source={{uri: `https://unsplash.it/150/150?image=25` }} style={dashSellerStyle.productImg}/>
+								<View style={dashSellerStyle.productDesc} >
+									<View>
+										<Text style= { dashSellerStyle.productName } >Backpack Black</Text>
+									</View>
+									<Text style= { dashSellerStyle.productPrice } >$299.00</Text>
+								</View>
+							</View>
+
+							<View style={dashSellerStyle.productItem} >
+							   		<Image source={{uri: `https://unsplash.it/150/150?image=26` }} style={dashSellerStyle.productImg}/>
+								<View style={dashSellerStyle.productDesc} >
+									<View>
+										<Text style= { dashSellerStyle.productName } >Backpack Black</Text>
+									</View>
+									<Text style= { dashSellerStyle.productPrice } >$199.00</Text>
+								</View>
+							</View>
+
+							<View style={dashSellerStyle.productItem} >
+							   		<Image source={{uri: `https://unsplash.it/150/150?image=27` }} style={dashSellerStyle.productImg}/>
+								<View style={dashSellerStyle.productDesc} >
+									<View>
+										<Text style= { dashSellerStyle.productName } >Backpack Black</Text>
+									</View>
+									<Text style= { dashSellerStyle.productPrice } >$399.00</Text>
+								</View>
+							</View>
+
+							<View style={dashSellerStyle.productItem} >
+							   		<Image source={{uri: `https://unsplash.it/150/150?image=28` }} style={dashSellerStyle.productImg}/>
+								<View style={dashSellerStyle.productDesc} >
+									<View>
+										<Text style= { dashSellerStyle.productName } >Backpack Black</Text>
+									</View>
+									<Text style= { dashSellerStyle.productPrice } >$299.00</Text>
+								</View>
+							</View>
+
+							<View style={dashSellerStyle.productItem} >
+							   		<Image source={{uri: `https://unsplash.it/150/150?image=29` }} style={dashSellerStyle.productImg}/>
+								<View style={dashSellerStyle.productDesc} >
+									<View>
+										<Text style= { dashSellerStyle.productName } >Backpack Black</Text>
+									</View>
+									<Text style= { dashSellerStyle.productPrice } >$299.00</Text>
+								</View>
+							</View>
+
+						
+						</ScrollView>
+
+					</View>
+				</View>
+
+				<View style={dashSellerStyle.contentRow}>
+					<View>
+						<Text style={dashSellerStyle.blockName}>FEATURED STORE</Text>
+					</View>
+
+					<View style={dashSellerStyle.featuredRow} >
+						<View style={dashSellerStyle.featuredBlock}>
+							<Text style={dashSellerStyle.featuredDesc}>
+								Your Style Guide to Winter Clothes Shopping with the founder of @mm_ShopBop
+							</Text>
+						</View>
+	
+						<View style={dashSellerStyle.featuredBlock}>
+							<Image source={{uri: `https://unsplash.it/250/250?image=39` }} style={dashSellerStyle.featuredImg}/>
+						</View>
+
+					</View>
+				</View>
+
+
+				<View style={dashSellerStyle.contentRow}>
+					<View>
+						<Text style={dashSellerStyle.blockName}>WE THOUGHT YOU MIGHT LIKE</Text>
+					</View>
+
+					<View style={dashSellerStyle.featuredRow} >
+						<View style={dashSellerStyle.featuredBlock}>
+							<Text style={dashSellerStyle.featuredDesc}>
+								Your Style Guide to Winter Clothes Shopping with the founder of @mm_ShopBop
+							</Text>
+						</View>
+				
+						<View style={dashSellerStyle.featuredBlock}>
+							<Image source={{uri: `https://unsplash.it/250/250?image=39` }} style={dashSellerStyle.featuredImg}/>
+						</View>
+
+					</View>
+
+
+				</View>
+
+
 
 					
 				</ScrollView>
@@ -79,23 +218,124 @@ export default class DashboardSeller extends Component {
 }
 
 const dashSellerStyle = StyleSheet.create({
+	featuredBlock: {
+		flex: 1,
+		height: 180,
+		borderWidth: 0.5,
+		borderColor: '#aaa',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	featuredImg: {
+		height: 180,
+		width: 180,
+		resizeMode: 'cover',
+	},
+	featuredDesc: {
+		textAlign: 'center',
+		padding: 13,
+		fontSize: 19,
+		fontStyle: 'italic',
+
+	},
+	featuredRow: {
+		flexDirection: 'row',
+	},
+	productPrice: {
+		paddingTop: 5,
+		color: '#783ab1',
+		textAlign:'center',
+		fontWeight: 'bold',
+	},
+	productName: {
+		color: 'black',
+		textAlign:'center',
+		fontWeight: 'bold',
+	},
+	productImg: {
+		height: 130,
+		width: 130,
+    	resizeMode: 'cover',
+	},
+	productItem: {
+		padding: 13,
+		borderWidth: 0.5,
+		borderColor: '#aaa',
+	},
+	productDesc: {
+		paddingTop: 5,
+		alignItems: 'center',
+	},
+	contentRow: {
+		marginTop: 10,
+	},
 	shopTitleView: {
 		justifyContent: 'center',
 		paddingHorizontal: 10,
 	},
+	productList: {
+		flexDirection: 'row',
+	},
+	dealDayButtonRow: {
+		flexDirection: 'row',
+		paddingTop: 10,
+	},
+	dealDayButton: {
+		backgroundColor: 'rgba(255, 255, 255, 0.15)',
+		paddingHorizontal: 15,
+		paddingVertical: 8,
+		borderWidth: 1,
+		borderColor: 'white',
+	},
+	dealDayContent: {
+		zIndex: 20,
+		flex: 1,
+		paddingHorizontal: 8,
+		justifyContent: 'center',
+		paddingBottom: 30,
+	},
+	blockName: {
+		fontStyle: 'italic',
+		paddingLeft: 8,
+	},
+	dealDay: {
+		height: 180,
+	},
+	shopperName: {
+		textAlign: 'center',
+		color: 'black',
+	},
+	dealDayImg: {
+		zIndex: 10,
+		flex: 1,
+    	resizeMode: 'cover',
+    	top: 0,
+    	left: 0,
+    	right: 0,
+    	bottom: 0,
+    	position: 'absolute',
+    },
 	container: {
 		flex: 1,
 		backgroundColor: `white`,
 	},
 	shopperImgProfile: {
-		flex: 2,
+		flex: 3,
 		alignItems: 'center',
 	},
 	shopperPropsProfile: {
-		flex: 1,
+		flex: 2,
 		alignItems: 'center',
 	},
+	textPropDesc: {
+		textAlign: 'center',
+		color: 'black',
+	},
+	textPropValue: {
+		color: 'black',
+		fontSize: 20,
+	},
 	alignCenter: {
-		borderWidth: 2,
+		alignItems: 'center',
 	}
 });

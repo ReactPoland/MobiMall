0fd5d3f806ba609b32763ca96836b9b78d0a8bb1
Tolik <talibasya@gmail.com>
2016-10-17 10:41:40 +0200

    #33 add setting view content to the side menu

--- components/ThemeUi.js
+++ components/ThemeUi.js
@@ -8,7 +8,7 @@ import {
 import { COLOR, ThemeProvider, ActionButton } from 'react-native-material-ui';
 // import { Drawer } from 'react-native-material-design';
 import { Avatar, Drawer, Divider, COLOR as cc, TYPO } from 'react-native-material-design';
-
+import Setting from '../routes/Setting'
 
 
 export default class ThemeUi extends Component {
@@ -46,7 +46,7 @@ export default class ThemeUi extends Component {
 
   render() {
     const { drawer, navigator } = this.state;
-    const navView = React.createElement(Navigation);
+    const navView = React.createElement(Setting);
 
 
 
@@ -86,123 +86,6 @@ export default class ThemeUi extends Component {
 };
 
 
-class Navigation extends Component {
-
-  changeScene(path, name) {
-      // const { drawer, navigator } = this.context;
-
-      // this.setState({
-          // route: path
-      // });
-      // navigator.to(path, name);
-      // drawer.closeDrawer();
-  }
-
-  render() {
-    
-    const route = "welcome";
-
-
-    return (
-
-      <Drawer theme='light'>
-          <Drawer.Header >
-              <View style={styles.header}>
-                  <Text style={[styles.text, cc.paperGrey50, TYPO.paperFontSubhead]}>React Native Material Design</Text>
-              </View>
-          </Drawer.Header>
-
-          <Drawer.Section
-              items={[{
-                  icon: 'home',
-                  value: 'Welcome',
-                  active: !route || route === 'welcome',
-                  onPress: () => this.changeScene('welcome'),
-                  onLongPress: () => this.changeScene('welcome')
-              }]}
-          />
-
-          <Drawer.Section
-              title="Components"
-              items={[{
-                  icon: 'face',
-                  value: 'Avatars',
-                  label: '12',
-                  active: route === 'avatars',
-                  onPress: () => this.changeScene('avatars'),
-                  onLongPress: () => this.changeScene('avatars')
-              }, {
-                  icon: 'label',
-                  value: 'Buttons',
-                  active: route === 'buttons',
-                  label: '8',
-                  onPress: () => this.changeScene('buttons'),
-                  onLongPress: () => this.changeScene('buttons')
-              }, {
-                  icon: 'check-box',
-                  value: 'Checkboxes',
-                  label: '10',
-                  active: route === 'checkboxes',
-                  onPress: () => this.changeScene('checkboxes'),
-                  onLongPress: () => this.changeScene('checkboxes')
-              }, {
-                  icon: 'label',
-                  value: 'Dividers',
-                  label: '10',
-                  active: route === 'dividers',
-                  onPress: () => this.changeScene('dividers'),
-                  onLongPress: () => this.changeScene('dividers')
-              }, {
-                  icon: 'label',
-                  value: 'Icon Toggles',
-                  label: 'NEW',
-                  active: route === 'icon-toggles',
-                  onPress: () => this.changeScene('icon-toggles'),
-                  onLongPress: () => this.changeScene('icon-toggles')
-              }, {
-                  icon: 'radio-button-checked',
-                  value: 'Radio Buttons',
-                  label: '8',
-                  active: route === 'radio-buttons',
-                  onPress: () => this.changeScene('radio-buttons'),
-                  onLongPress: () => this.changeScene('radio-buttons')
-              },
-               //{
-               //   icon: 'list',
-               //   value: 'List',
-               //   label: 'NEW',
-               //   active: route === 'list',
-               //   onPress: () => this.changeScene('list'),
-               //   onLongPress: () => this.changeScene('list')
-               //},
-              {
-                  icon: 'label',
-                  value: 'Subheaders',
-                  label: '4',
-                  active: route === 'subheaders',
-                  onPress: () => this.changeScene('subheaders'),
-                  onLongPress: () => this.changeScene('subheaders')
-              }]}
-          />
-          <Divider style={{ marginTop: 8 }} />
-          <Drawer.Section
-              title="Config"
-              items={[{
-                  icon: 'invert-colors',
-                  value: 'Change Theme',
-                  label: '24',
-                  active: route === 'themes',
-                  onPress: () => this.changeScene('themes'),
-                  onLongPress: () => this.changeScene('themes')
-              }]}
-          />
-
-      </Drawer>
-
-    )
-  }
-
-}
 
 const styles = {
     header: {
